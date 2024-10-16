33effe9e13109549d2f4554e604ff5a2cad9eeaa
alden2004 <152677333+alden2004@users.noreply.github.com>
2024-10-16 09:40:48 +0700

    Add files via upload

---
+++ Latihan.py
@@ -0,0 +1,10 @@
+class Manusia:
+    name = ""
+    umur = 0
+
+manusiaRussia = Manusia()
+
+manusiaRussia.umur = 20
+manusiaRussia.name = "Manusia Russia"
+
+print(f"Name: {manusiaRussia.name}, umur: {manusiaRussia.umur}")
